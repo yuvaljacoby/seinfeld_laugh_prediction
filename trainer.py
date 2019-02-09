832a55efd90614253e4a42014088191bcdb913d6
DanKufra <dankufra@gmail.com>
2019-02-09 13:39:34 +0200

    CNN test

--- trainer.py
+++ trainer.py
@@ -40,39 +40,6 @@ MIN_DOCUMENT_FREQUENCY = 2
 # will be truncated.
 MAX_SEQUENCE_LENGTH = 20
 
-def sequence_vectorize(train_texts, val_texts):
-    """Vectorizes texts as sequence vectors.
-
-    1 text = 1 sequence vector with fixed length.
-
-    # Arguments
-        train_texts: list, training text strings.
-        val_texts: list, validation text strings.
-
-    # Returns
-        x_train, x_val, word_index: vectorized training and validation
-            texts and word index dictionary.
-    """
-    # Create vocabulary with training texts.
-    tokenizer = text.Tokenizer(num_words=TOP_K)
-    tokenizer.fit_on_texts(train_texts)
-
-    # Vectorize training and validation texts.
-    x_train = tokenizer.texts_to_sequences(train_texts)
-    x_val = tokenizer.texts_to_sequences(val_texts)
-
-    # Get max sequence length.
-    max_length = len(max(x_train, key=len))
-    if max_length > MAX_SEQUENCE_LENGTH:
-        max_length = MAX_SEQUENCE_LENGTH
-
-    # Fix sequence length to max value. Sequences shorter than the length are
-    # padded in the beginning and sequences longer are truncated
-    # at the beginning.
-    x_train = sequence.pad_sequences(x_train, maxlen=max_length)
-    x_val = sequence.pad_sequences(x_val, maxlen=max_length)
-    return x_train, x_val, tokenizer.word_index
-
 def ngram_vectorize(train_texts, train_labels, val_texts):
     """Vectorizes texts as n-gram vectors.
 
@@ -133,6 +100,92 @@ def mlp_model(layers, units, dropout_rate, input_shape):
     model.add(Dense(units=op_units, activation=op_activation))
     return model
 
+def train_ngram_model(train_texts, train_labels, val_texts, val_labels, learning_rate=1e-3, epochs=100,
+                      batch_size=128, layers=2, units=64, dropout_rate=0.2):
+    """Trains n-gram model on the given dataset.
+
+    # Arguments
+        data: tuples of training and test texts and labels.
+        learning_rate: float, learning rate for training model.
+        epochs: int, number of epochs.
+        batch_size: int, number of samples per batch.
+        layers: int, number of `Dense` layers in the model.
+        units: int, output dimension of Dense layers in the model.
+        dropout_rate: float: percentage of input to drop at Dropout layers.
+
+    # Raises
+        ValueError: If validation data has label values which were not seen
+            in the training data.
+    """
+
+    # Verify that validation labels are in the same range as training labels.
+    num_classes = 2
+
+    # Vectorize texts.
+    x_train, x_val = ngram_vectorize(train_texts, train_labels, val_texts)
+
+    # Create model instance.
+    model = mlp_model(layers=layers,
+                      units=units,
+                      dropout_rate=dropout_rate,
+                      input_shape=x_train.shape[1:])
+
+    # Compile model with learning parameters.
+    loss = 'binary_crossentropy'
+
+    optimizer = tf.keras.optimizers.Adam(lr=learning_rate)
+    model.compile(optimizer=optimizer, loss=loss, metrics=['acc'])
+
+    # Create callback for early stopping on validation loss. If the loss does
+    # not decrease in two consecutive tries, stop training.
+    callbacks = [tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=2)]
+
+    # Train and validate model.
+    history = model.fit(x_train, train_labels, epochs=epochs, callbacks=callbacks, validation_data=(x_val, val_labels),
+                        verbose=2, batch_size=batch_size)
+
+    # Print results.
+    history = history.history
+    print('Validation accuracy: {acc}, loss: {loss}'.format(
+            acc=history['val_acc'][-1], loss=history['val_loss'][-1]))
+
+    # Save model.
+    model.save('tfidf_mlp_model.h5')
+    return history['val_acc'][-1], history['val_loss'][-1]
+
+def sequence_vectorize(train_texts, val_texts):
+    """Vectorizes texts as sequence vectors.
+
+    1 text = 1 sequence vector with fixed length.
+
+    # Arguments
+        train_texts: list, training text strings.
+        val_texts: list, validation text strings.
+
+    # Returns
+        x_train, x_val, word_index: vectorized training and validation
+            texts and word index dictionary.
+    """
+    # Create vocabulary with training texts.
+    tokenizer = text.Tokenizer(num_words=TOP_K)
+    tokenizer.fit_on_texts(train_texts)
+
+    # Vectorize training and validation texts.
+    x_train = tokenizer.texts_to_sequences(train_texts)
+    x_val = tokenizer.texts_to_sequences(val_texts)
+
+    # Get max sequence length.
+    max_length = len(max(x_train, key=len))
+    if max_length > MAX_SEQUENCE_LENGTH:
+        max_length = MAX_SEQUENCE_LENGTH
+
+    # Fix sequence length to max value. Sequences shorter than the length are
+    # padded in the beginning and sequences longer are truncated
+    # at the beginning.
+    x_train = sequence.pad_sequences(x_train, maxlen=max_length)
+    x_val = sequence.pad_sequences(x_val, maxlen=max_length)
+    return x_train, x_val, tokenizer.word_index
+
 def sepcnn_model(blocks,
                  filters,
                  kernel_size,
@@ -140,7 +193,6 @@ def sepcnn_model(blocks,
                  dropout_rate,
                  pool_size,
                  input_shape,
-                 num_classes,
                  num_features,
                  use_pretrained_embedding=False,
                  is_embedding_trainable=False,
@@ -155,7 +207,6 @@ def sepcnn_model(blocks,
         dropout_rate: float, percentage of input to drop at Dropout layers.
         pool_size: int, factor by which to downscale input at MaxPooling layer.
         input_shape: tuple, shape of input to the model.
-        num_classes: int, number of output classes.
         num_features: int, number of words (embedding input dimension).
         use_pretrained_embedding: bool, true if pre-trained embedding is on.
         is_embedding_trainable: bool, true if embedding layer is trainable.
@@ -213,62 +264,8 @@ def sepcnn_model(blocks,
     model.add(Dense(op_units, activation=op_activation))
     return model
 
-def train_ngram_model(train_texts, train_labels, val_texts, val_labels, learning_rate=1e-3, epochs=100,
-                      batch_size=128, layers=2, units=64, dropout_rate=0.2):
-    """Trains n-gram model on the given dataset.
-
-    # Arguments
-        data: tuples of training and test texts and labels.
-        learning_rate: float, learning rate for training model.
-        epochs: int, number of epochs.
-        batch_size: int, number of samples per batch.
-        layers: int, number of `Dense` layers in the model.
-        units: int, output dimension of Dense layers in the model.
-        dropout_rate: float: percentage of input to drop at Dropout layers.
-
-    # Raises
-        ValueError: If validation data has label values which were not seen
-            in the training data.
-    """
-
-    # Verify that validation labels are in the same range as training labels.
-    num_classes = 2
-
-    # Vectorize texts.
-    x_train, x_val = ngram_vectorize(train_texts, train_labels, val_texts)
-
-    # Create model instance.
-    model = mlp_model(layers=layers,
-                      units=units,
-                      dropout_rate=dropout_rate,
-                      input_shape=x_train.shape[1:])
-
-    # Compile model with learning parameters.
-    loss = 'binary_crossentropy'
-
-    optimizer = tf.keras.optimizers.Adam(lr=learning_rate)
-    model.compile(optimizer=optimizer, loss=loss, metrics=['acc'])
-
-    # Create callback for early stopping on validation loss. If the loss does
-    # not decrease in two consecutive tries, stop training.
-    callbacks = [tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=2)]
-
-    # Train and validate model.
-    history = model.fit(x_train, train_labels, epochs=epochs, callbacks=callbacks, validation_data=(x_val, val_labels),
-                        verbose=2, batch_size=batch_size)
-
-    # Print results.
-    history = history.history
-    print('Validation accuracy: {acc}, loss: {loss}'.format(
-            acc=history['val_acc'][-1], loss=history['val_loss'][-1]))
-
-    # Save model.
-    model.save('tfidf_mlp_model.h5')
-    return history['val_acc'][-1], history['val_loss'][-1]
-
-
 def train_sepCNN_model(train_texts, train_labels, val_texts, val_labels, learning_rate=1e-3, epochs=100,
-                      batch_size=128, layers=2, units=64, dropout_rate=0.2):
+                      batch_size=128, blocks=2, filters=64, kernel_size=5, embedding_dim=128, pool_size=2, dropout_rate=0.2):
     """Trains n-gram model on the given dataset.
 
     # Arguments
@@ -284,18 +281,19 @@ def train_sepCNN_model(train_texts, train_labels, val_texts, val_labels, learnin
         ValueError: If validation data has label values which were not seen
             in the training data.
     """
-
-    # Verify that validation labels are in the same range as training labels.
-    num_classes = 2
 
     # Vectorize texts.
     x_train, x_val = sequence_vectorize(train_texts, val_texts)
 
     # Create model instance.
-    model = sepcnn_model(layers=layers,
-                         units=units,
+    model = sepcnn_model(blocks=blocks,
+                         filters=filters,
+                         kernel_size=kernel_size,
+                         embedding_dim=embedding_dim,
                          dropout_rate=dropout_rate,
-                         input_shape=x_train.shape[1:])
+                         pool_size=pool_size,
+                         input_shape=x_train.shape[1:],
+                         num_features=MAX_SEQUENCE_LENGTH)
 
     # Compile model with learning parameters.
     loss = 'binary_crossentropy'
@@ -328,5 +326,5 @@ if __name__ == "__main__":
     train_df = df.sample(frac=0.8,random_state=200)
     test_df = df.drop(train_df.index)
 
-    history_val_acc, history_val_loss = train_ngram_model(train_df.txt, train_df.is_funny.astype(np.float32), test_df.txt, test_df.is_funny.astype(np.float32))
+    # history_val_acc, history_val_loss = train_ngram_model(train_df.txt, train_df.is_funny.astype(np.float32), test_df.txt, test_df.is_funny.astype(np.float32))
     history_val_acc_sepCNN, history_val_loss_sepCNN = train_sepCNN_model(train_df.txt, train_df.is_funny.astype(np.float32), test_df.txt, test_df.is_funny.astype(np.float32))
